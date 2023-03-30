5be56fac3f4f691ba8095d1c46b7398079510f04
Abdusalam01 <92310391+Abdusalam01@users.noreply.github.com>
2023-03-30 22:14:01 +0300

    Java

--- Siniflar/Referans_dizisi.java
+++ Siniflar/Referans_dizisi.java
@@ -1,5 +1,24 @@
 public class Referans_dizisi {
     public static void main(String[] args) {
+        Sinif[] dizi = new Sinif[50];
+
+        for(int i = 0 ; i < 50; i++)
+            dizi[i] = new Sinif();
         
+        System.out.println("dizi[25] den sonra kaçtane eleman olusturuldu: " + dizi[25].BendenSonraKacNesne());
+    }
+}
+
+class Sinif {
+    static int sayi = 0;
+    int indis = 0;
+
+    Sinif(){
+        sayi++;
+        indis = sayi;
+    }
+
+    int BendenSonraKacNesne(){
+        return sayi - indis;
     }
 }
